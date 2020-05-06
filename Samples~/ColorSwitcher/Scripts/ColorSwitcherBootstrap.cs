88f613bd5e2a8f6a9c457550be63e3d30207bdb7
Jochen Heckl <jochen.heckl@gmail.com>
2020-05-06 16:41:44 +0200

    fixed an other using error where de.JochenHeckl was missing

--- Samples~/ColorSwitcher/Scripts/ColorSwitcherBootstrap.cs
+++ Samples~/ColorSwitcher/Scripts/ColorSwitcherBootstrap.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using IoCLight;
+using de.JochenHeckl.IoCLight;
 using UnityEngine;
 
 public class ColorSwitcherBootstrap : BootstrapBase
