48e74ac69de932ee2df5f6a1263f77d13ec8d4ed
jochen <jochen.heckl@gmail.com>
2024-02-10 09:44:30 +0100

    Fixes broken usings

--- Runtime/Container.cs
+++ Runtime/Container.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 
-using PlasticPipe.PlasticProtocol.Server;
-
 namespace de.JochenHeckl.Unity.IoCLight
 {
     public class Container : IContainer
     {
         private readonly List<ITypeBinding> typeBindings = new List<ITypeBinding>();
 
-        public Container() { 
+        public Container()
+        {
             RegisterInstance(this).As<IContainer>();
         }
 
@@ -18,74 +17,84 @@ namespace de.JochenHeckl.Unity.IoCLight
         {
             var binding = new TypeBindingBase()
             {
-                LookupType = typeof( InstanceType ),
-                ResolveType = typeof( InstanceType )
+                LookupType = typeof(InstanceType),
+                ResolveType = typeof(InstanceType)
             };
 
-            typeBindings.Add( binding );
+            typeBindings.Add(binding);
 
             return binding;
         }
 
-        public ITypeBinding RegisterInstance<InstanceType>( InstanceType instance )
+        public ITypeBinding RegisterInstance<InstanceType>(InstanceType instance)
         {
-            var binding = new TypeBindingBase( instance )
+            var binding = new TypeBindingBase(instance)
             {
                 SingleInstance = true,
-                LookupType = typeof( InstanceType ),
-                ResolveType = typeof( InstanceType )
+                LookupType = typeof(InstanceType),
+                ResolveType = typeof(InstanceType)
             };
 
-            typeBindings.Add( binding );
+            typeBindings.Add(binding);
 
             return binding;
         }
 
-		public ITypeBinding RegisterFactory<ProductType>( Func<IContainer, ProductType> producer )
+        public ITypeBinding RegisterFactory<ProductType>(Func<IContainer, ProductType> producer)
         {
-			var binding = new FactoryBinding<ProductType>( producer )
-			{
-				SingleInstance = false,
-				LookupType = typeof( ProductType )
-			};
-
-			typeBindings.Add( binding );
-
-			return binding;
-		}
-
-		public InstanceType Resolve<InstanceType>() where InstanceType : class
-        {
-            var typeBinding = typeBindings.FirstOrDefault( x => typeof( InstanceType ).IsAssignableFrom( x.LookupType ) );
-
-            if ( typeBinding == null )
+            var binding = new FactoryBinding<ProductType>(producer)
             {
-                throw new InvalidOperationException( $"Failed to resolve type {typeof(InstanceType)}." +
-					$" Make sure you registered  {typeof( InstanceType )} with your container?" );
-            }
+                SingleInstance = false,
+                LookupType = typeof(ProductType)
+            };
 
-            return typeBinding.Resolve<InstanceType>( this );
+            typeBindings.Add(binding);
+
+            return binding;
         }
 
-        public InstanceType[] ResolveAll<InstanceType>() where InstanceType : class
+        public InstanceType Resolve<InstanceType>()
+            where InstanceType : class
         {
-            return typeBindings
-                .Where( x => typeof( InstanceType ).IsAssignableFrom( x.LookupType ) )
-                .Select( x => x.Resolve<InstanceType>( this ) )
-                .ToArray();
-        }
-
-        public object Resolve( Type typeOfInstanceType )
-        {
-            var typeBinding = typeBindings.FirstOrDefault( x => typeOfInstanceType.IsAssignableFrom( x.LookupType ) );
+            var typeBinding = typeBindings.FirstOrDefault(x =>
+                typeof(InstanceType).IsAssignableFrom(x.LookupType)
+            );
 
             if (typeBinding == null)
             {
-                throw new InvalidOperationException( $"{typeOfInstanceType.Name} can not be resolved." +
-					$" Make sure you registered {typeOfInstanceType} with your container?" );
+                throw new InvalidOperationException(
+                    $"Failed to resolve type {typeof(InstanceType)}."
+                        + $" Make sure you registered  {typeof(InstanceType)} with your container?"
+                );
             }
 
-            return typeBinding.Resolve( this );
+            return typeBinding.Resolve<InstanceType>(this);
+        }
+
+        public InstanceType[] ResolveAll<InstanceType>()
+            where InstanceType : class
+        {
+            return typeBindings
+                .Where(x => typeof(InstanceType).IsAssignableFrom(x.LookupType))
+                .Select(x => x.Resolve<InstanceType>(this))
+                .ToArray();
+        }
+
+        public object Resolve(Type typeOfInstanceType)
+        {
+            var typeBinding = typeBindings.FirstOrDefault(x =>
+                typeOfInstanceType.IsAssignableFrom(x.LookupType)
+            );
+
+            if (typeBinding == null)
+            {
+                throw new InvalidOperationException(
+                    $"{typeOfInstanceType.Name} can not be resolved."
+                        + $" Make sure you registered {typeOfInstanceType} with your container?"
+                );
+            }
+
+            return typeBinding.Resolve(this);
         }
 
         public void Terminate()
